f8841d71e7797c5e4a389dbca3f7493b78e745e4
liyuxuan <liyuxuan12345678@outlook.com>
2024-09-17 03:52:42 -0400

    a

---
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using CreatorKitCode;
+
+public class SpawnerSample : MonoBehaviour
+{
+    public GameObject ObjectToSpawn;
+
+    void Start()
+    {
+        int angle = 15;
+        Vector3 spawnPosition = transform.position;
+
+        
+        Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;  // Rotate the direction vector x by 15 degrees around the y axis.
+        spawnPosition = transform.position + direction * 2;
+        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+
+        angle = 15;
+        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        spawnPosition = transform.position + direction * 4;
+        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+
+        angle = 15;
+        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        spawnPosition = transform.position + direction * 6;
+        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+    }
+}
+
